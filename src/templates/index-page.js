29f2ecea214bb47f3ad208c0050ae15752ab1a43
grimgoon <alexander@hermansson.us>
2019-09-07 16:39:47 +0200

    Test - update index page to markdown

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,6 +5,7 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+import Content, { HTMLContent } from '../components/Content'
 
 export const IndexPageTemplate = ({
 
