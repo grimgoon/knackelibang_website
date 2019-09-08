1241fc3c7b3030f4f8fe041974f02c13bd257850
grimgoon <alexander@hermansson.us>
2019-09-08 10:20:14 +0200

    Latest blogs - Index page

--- src/pages/index.md
+++ src/pages/index.md
@@ -5,7 +5,8 @@ mainpitch:
 ---
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Odio eu feugiat pretium nibh. Odio tempor orci dapibus ultrices in iaculis nunc. Consequat ac felis donec et odio pellentesque. Fermentum odio eu feugiat pretium. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Aliquet sagittis id consectetur purus ut faucibus pulvinar. In iaculis nunc sed augue. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros.
 
-
+&nbsp;
+&nbsp;
 
 Sodales ut etiam sit amet nisl purus in mollis. Hac habitasse platea dictumst vestibulum rhoncus. Viverra accumsan in nisl nisi scelerisque eu. Fames ac turpis egestas integer eget aliquet nibh praesent. Faucibus purus in massa tempor nec feugiat nisl pretium fusce. Ut placerat orci nulla pellentesque dignissim enim sit. Purus in massa tempor nec feugiat nisl pretium fusce. Tincidunt arcu non sodales neque sodales ut etiam sit amet. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Accumsan lacus vel facilisis volutpat est. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Convallis convallis tellus id interdum. Consectetur lorem donec massa sapien faucibus et molestie ac. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Condimentum lacinia quis vel eros donec ac odio.
 
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,42 +1,68 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import GatsbyImage from "gatsby-image";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
-import Content, { HTMLContent } from '../components/Content'
+import Layout from "../components/Layout";
+import BlogRoll from "../components/BlogRoll";
+import Content, { HTMLContent } from "../components/Content";
 
 export const IndexPageTemplate = ({
   content,
   contentComponent,
   mainpitch,
+  latestBlogs
 }) => {
-  const PageContent = contentComponent || Content
-
+  const PageContent = contentComponent || Content;
+  console.log(latestBlogs);
   return (
-  <div className="home">
+    <div className="home">
       <h1>{mainpitch.title}</h1>
-      <PageContent className="content" content={content} />
-  </div>
-)
-}
+      <PageContent className="homeContent" content={content} />
+      <h1 className="latestBlogsTitle">Latest Blogs</h1>
+      <div className="latestBlogs">
+        {latestBlogs.map((latestBlog, index) => {
+          const blog = latestBlog.node.frontmatter;
+          return (
+            <div className="item" key={index}>
+              <div className="image">
+                <GatsbyImage
+                  key={index}
+                  fixed={blog.featuredimage.childImageSharp.fixed}
+                />
+              </div>
+              <div className="text">
+                <h1>{blog.title}</h1>
+                <p className="date">{blog.date}</p>
+                <Link to={latestBlog.node.fields.slug}>
+                  <div className="button">
+                    Read More
+                  </div>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 const IndexPage = ({ data }) => {
   const { index, latestBlogs } = data;
-  console.log(latestBlogs);
   return (
     <Layout>
       <IndexPageTemplate
         content={index.html}
         contentComponent={HTMLContent}
         mainpitch={index.frontmatter.mainpitch}
+        latestBlogs={latestBlogs.edges}
       />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -47,12 +73,14 @@ export const pageQuery = graphql`
           title
         }
       }
-    } 
-    latestBlogs: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {frontmatter: {templateKey: {eq: "blog-post"}}}, limit: 2) {
+    }
+    latestBlogs: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+      limit: 2
+    ) {
       edges {
         node {
-          excerpt(pruneLength: 400)
-          id
           fields {
             slug
           }
@@ -61,9 +89,16 @@ export const pageQuery = graphql`
             templateKey
             date(formatString: "MMMM DD, YYYY")
             featuredpost
+            featuredimage {
+              childImageSharp {
+                fixed(width: 360, height: 203, quality: 100) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
           }
         }
       }
     }
   }
-`
+`;
