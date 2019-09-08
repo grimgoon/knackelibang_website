9252c52b4c97f94e1cc886f46c3c2f3f9293dc0c
grimgoon <alexander@hermansson.us>
2019-09-08 11:21:19 +0200

    Small update to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,9 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    <div style={{margin: "80px 0px"}}>
       <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>The page you were looking for could not be found!</p>
     </div>
   </Layout>
 )
